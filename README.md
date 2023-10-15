4eee70de6dfa306100737b8a8aa3c913b4126376
haunt1603 <haunt1603@gmail.com>
2023-10-15 21:45:27 +0700

    commit 15/10

---
+++ README.md
@@ -0,0 +1,4 @@
+## S3 Bucket: 
+http://bucket5773.s3-website-us-east-1.amazonaws.com/
+## Elastic Beanstalk: 
+http://udagram-api-dev-env.eba-ruswqm9r.us-east-1.elasticbeanstalk.com/
---
+++ udagram/set_env.sh
@@ -0,0 +1,13 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+set POSTGRES_USERNAME= postgres
+set POSTGRES_PASSWORD= postgres123!
+set POSTGRES_HOST= postgres.cgiwanscgppv.us-east-1.rds.amazonaws.com
+set POSTGRES_DB= postgres
+set AWS_BUCKET= arn:aws:s3:::bucket9372
+set AWS_REGION= us-east-1
+set AWS_PROFILE= default
+set JWT_SECRET= mysecretstring
+set URL= http://localhost:8080
+set AWS_ACCESS_KEY_ID= ASIA5UOJQJWQA27ZKMGR
+set AWS_SECRET_ACCESS_KEY= 27gGHlyzNENCwb3TbbR6loMNI7E2b8WCh8BBb8iP
